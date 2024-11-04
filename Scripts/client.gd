895bd3c72b182e05736147cb89b84899e1bc0a46
N07H1NG <nikitos09012004@mail.ru>
2024-11-04 06:29:04 +0300

    clients spawn and come

--- Scripts/client.gd
+++ Scripts/client.gd
@@ -1,14 +1,16 @@
-extends Node3D
+class_name  Client3D extends Node3D
 
 @onready var head_sprite = $Head
 @onready var body_sprite = $Body
 @onready var hair_sprite = $Hair
 
+var path : PathFollow3D
+
 
 func _select_character():
 	var head_num = randi() % 5
 	var body_num = randi() % 2
-	var hair_num = randi() % 5
+	var hair_num = randi() % 6
 	
 	var head_tex_path = "res://Resources/Sprites/Characters/Heads/head_%d.PNG"
 	var body_tex_path = "res://Resources/Sprites/Characters/Bodies/body_%d.PNG"
@@ -31,3 +33,50 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func come_in() -> void:
+	var start_rotation = rotation
+	var start_position = position
+	var p = 0
+	var tree = get_tree()
+	var speed = randf()/4+0.5
+	await tree.create_timer(2).timeout
+	while (p<1):
+		p+=get_process_delta_time()*speed;
+		if p>1:
+			p = 1.0
+		rotation = start_rotation + Vector3(0,0,0.2*sin(4*PI*p))
+		position = start_position + Vector3(0,p/5+0.2+0.2*sin(8*PI*p-PI/2),0)
+		path.progress_ratio = p
+		await tree.process_frame
+	await tree.create_timer(2).timeout
+	leave()
+		
+
+
+func leave() -> void:
+	var start_rotation = rotation
+	
+	var p = 0.0
+	var tree = get_tree()
+	var speed = 0.6
+	await tree.create_timer(2).timeout
+	while (p<1):
+		p+=get_process_delta_time()*speed;
+		if p>1:
+			p = 1.0
+		rotation = start_rotation + Vector3(0,0,0.2*p*sin(4*PI*p))
+		await tree.process_frame
+	p = 0.0
+	start_rotation = rotation
+	while (p<1):
+		p+=get_process_delta_time()*speed;
+		if p>1:
+			p = 1.0
+		rotation = start_rotation + Vector3(4*p*p,0,0.2*(1-p)*sin((4-2*p)*PI*p))
+		await tree.process_frame
+		
+		
+	
+	
